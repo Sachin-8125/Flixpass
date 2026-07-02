5514742d939582a602b2923d05c8cd72a443f06d
Sachin-8125 <devinewolf7@gmail.com>
2026-07-02 19:08:15 +0530

    first commit

---
+++ backend/target/debug/backend.d
@@ -0,0 +1,1 @@
+C:\Projects\Flixpass\backend\target\debug\backend.exe: C:\Projects\Flixpass\backend\src\main.rs
---
+++ backend/target/debug/deps/backend.d
@@ -0,0 +1,5 @@
+C:\Projects\Flixpass\backend\target\debug\deps\backend.d: src\main.rs
+
+C:\Projects\Flixpass\backend\target\debug\deps\backend.exe: src\main.rs
+
+src\main.rs:
